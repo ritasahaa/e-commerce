fe370947d6fd0fbb311086d52931d4a7843acf3f
ritasahaa <ritasah1602@gmail.com>
2025-01-12 21:04:31 +0530

    Give padding in card_detail section

--- src/components/CardsDetail.jsx
+++ src/components/CardsDetail.jsx
@@ -77,7 +77,7 @@ const CardsDetail = () => {
                             <p> <strong>Dishes</strong> : {ele.address} </p>
                             <p> <strong>Total</strong> : ₹ {ele.price * ele.qnty} </p>
 
-                            <div className="mt-5 d-flex justify-content-between align-items-center" style={{width:100,cursor:"pointer",background:"#ddd",color:"#111"}}>
+                            <div className="mt-5 d-flex justify-content-between align-items-center" style={{width:100,padding:"2px 12px", cursor:"pointer",background:"#ddd",color:"#111"}}>
                               <span style={{fontSize:24}} onClick={ele.qnty <=1 ? ()=>dlt(ele.id) : ()=>remove(ele)}> - </span> 
                               <span style={{fontSize:20 }}> {ele.qnty} </span>
                               <span style={{fontSize:24}} onClick={()=>send(ele)}> + </span>
