ba910f7f8733ee4acb5797144e856ee77009759a
ritasahaa <ritasah1602@gmail.com>
2025-01-19 18:56:57 +0530

    fix minor bug

--- src/components/Buy.jsx
+++ src/components/Buy.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Buy = () => {
   return (
     <div className="d-flex justify-content-center align-items-center" style={{height:"80vh"}}>
-      <img src="/Images/scanner.jpeg" style={{ width: "400px",height: "400px" }}/>
+      <img src="/Images/scanner.jpeg" style={{ width: "400px", maxWidth:"80%",height: "400px" }}/>
     </div>
   )
 }
